13e9777b5f565c2d31b34a1d51f9db3f5eb72eef
SharonAquilla05 <162286887+SharonAquilla05@users.noreply.github.com>
2024-04-02 20:18:23 +0300

    Update foxy.js

--- foxy.js
+++ foxy.js
@@ -2,10 +2,10 @@
 // the rest are uppercase
 // tHE qUICK bROWN fOX'
 
-let string = 'The Quick Brown Fox'
-function caseSwap(string){
-    word =string.split('');
+let text = 'The Quick Brown Fox'
+function caseSwap(text){
+    word =text.split('');
     return word.map( i => {
         return i[0].tolowercase() +(i.slice(1)).touppercase()
     }).join("");
-}console.log(caseSwap(string))
+}console.log(caseSwap(text))
